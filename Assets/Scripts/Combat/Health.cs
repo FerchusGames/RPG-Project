fc6d0814662b49310e453a64a312a1fe0eda55b3
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2023-09-17 15:34:17 -0600

    Trigger Death Animation

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,15 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
-        [SerializeField] private float _health = 100f;
+        [SerializeField] private float _healthPoints = 100f;
+        
+        private Animator _animator;
+        
+        private static int AP_DEATH_TRIGGER = Animator.StringToHash("die");
+        private bool _isDead = false;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
 
         public void TakeDamage(float damage)
         {
-            _health = Mathf.Max(_health - damage, 0);
-            Debug.Log(_health);
+            _healthPoints = Mathf.Max(_healthPoints - damage, 0);
+            Debug.Log(_healthPoints);
+            if (_healthPoints == 0 && !_isDead)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            _isDead = true;
+            _animator.SetTrigger(AP_DEATH_TRIGGER);
         }
     }
 }
