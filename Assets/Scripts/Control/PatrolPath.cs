9b46fdec6dfbc9173d93541793e272f0476c86bb
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2023-09-18 14:12:39 -0600

    Drawing lines for PatrolPath loop

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -11,9 +11,26 @@ namespace RPG.Control
         {
             for (int i = 0; i < transform.childCount; i++)
             {
-                Gizmos.color = Color.cyan;
-                Gizmos.DrawSphere(transform.GetChild(i).position, _waypointGizmoRadius);
+                int j = GetNextIndex(i);
+                
+                Gizmos.DrawSphere(GetWaypoint(i), _waypointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
             }
+        }
+
+        private int GetNextIndex(int i)
+        {
+            if (i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+            
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
         }
     }
 }
