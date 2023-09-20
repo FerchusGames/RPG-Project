d39cbe25d7276203b40a3634508c7587ba5a2a17
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2023-09-20 12:09:08 -0600

    Trigger Cinemachine Sequence

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private PlayableDirector _playableDirector;
+
+        private bool _alreadyTriggered = false;
+        
+        private void Awake()
+        {
+            _playableDirector = GetComponent<PlayableDirector>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!_alreadyTriggered && other.gameObject.CompareTag("Player"))
+            {
+                _alreadyTriggered = true;
+                _playableDirector.Play();
+            }
+        }
+    }
+}
