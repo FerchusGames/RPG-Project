d8ec673c5fceb56d210115aae8b6338d95e620ab
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2023-09-25 14:06:14 -0600

    Cross Scene Referencing
    For Portal connections

--- Assets/Scripts/Scene Management/Portal.cs
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
@@ -5,12 +7,60 @@ namespace RPG
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+        
+        [SerializeField] private Transform _spawnPoint;
+        
+        [SerializeField] private int _sceneToLoad = -1;
+        [SerializeField] private DestinationIdentifier _destination;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                SceneManager.LoadScene("Sandbox 2");
+                StartCoroutine(Transition());
             }
+        }
+
+        private IEnumerator Transition()
+        {
+            if (_sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load not set.");
+                yield break;
+            }
+            
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(_sceneToLoad);
+            
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal._spawnPoint.position;
+            player.transform.rotation = otherPortal._spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                if (portal._destination != _destination) continue;
+                 
+                return portal;
+            }
+
+            return null;
         }
     }
 }
