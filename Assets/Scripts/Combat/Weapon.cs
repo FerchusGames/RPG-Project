2d1b2b71474be4c1818df86bf35cd0f3b46af9c2
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2024-02-07 17:39:46 -0600

    119. Destroy Old Weapon

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -6,6 +6,8 @@ namespace RPG.Combat
     [CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/Make New Weapon", order = 0)]
     public class Weapon : ScriptableObject
     {
+        private const string WEAPON_NAME = "Weapon";
+
         [SerializeField]
         private GameObject _equippedPrefab = null;
 
@@ -26,15 +28,34 @@ namespace RPG.Combat
 
         public void Spawn(Transform rightHand, Transform leftHand, Animator animator)
         {
+            DestroyOldweapon(rightHand, leftHand);
+
             if (_equippedPrefab != null)
             {
-                Instantiate(_equippedPrefab, GetHandTransform(rightHand, leftHand));
+                GameObject weapon = Instantiate(_equippedPrefab, GetHandTransform(rightHand, leftHand));
+                weapon.name = WEAPON_NAME;
             }
 
             if (_animatorOverrideController != null)
             {
                 animator.runtimeAnimatorController = _animatorOverrideController;
             }
+        }
+
+        private void DestroyOldweapon(Transform rightHand, Transform leftHand)
+        {
+            Transform oldWeapon = rightHand.Find(WEAPON_NAME);
+            if (oldWeapon == null)
+            {
+                oldWeapon = leftHand.Find(WEAPON_NAME);
+                if (oldWeapon == null)
+                {
+                    return;
+                }
+            }
+
+            oldWeapon.name = "DESTROYING";
+            Destroy(oldWeapon.gameObject);
         }
 
         private Transform GetHandTransform(Transform rightHand, Transform leftHand)
